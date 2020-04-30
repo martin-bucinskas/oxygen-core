c93ec40a86d1b51e064b9cbecdd8b76a9f62e31e
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2020-04-30 20:41:02 +0100

    Renamed class

--- src/main/java/martinb/oxygen/oxygencore/OxygenCoreApplication.java
+++ src/main/java/martinb/oxygen/oxygencore/OxygenCoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OxygencoreApplication {
+public class OxygenCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OxygencoreApplication.class, args);
+		SpringApplication.run(OxygenCoreApplication.class, args);
 	}
 
 }
